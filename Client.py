107e1d26f264beebe7ef250de87491a6488a528e
mabd <mabd@itu.dk>
2019-10-25 14:22:55 +0200

    Rerolled back to UDP

--- Client.py
+++ Client.py
@@ -1,67 +1,30 @@
 import socket
 import time
 import threading
-import sys
 from configparser import ConfigParser
 
 configur = ConfigParser()
 configur.read('opt.conf')
 heartBeat = configur.get('ServerSettings', 'KeepAlive')
 
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-hostname = socket.gethostbyname(socket.gethostname())
-port = 10000
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+
+hostname = socket.gethostname()
+IPAddr = socket.gethostbyname(hostname)
+server_address = (IPAddr, 10000)
+
 
 connectionCode = 'com-0: '
 connected = False
 heartBeatIntervalCheck = 0
-
-def main():
-    global connected
-    global heartBeatIntervalCheck
-
-    try:
-        sock.connect((hostname, port))
-    except:
-        print("Connection Error")
-        sys.exit()
-
-    try:
-        print('Sending IP for Syn')
-        sock.send((connectionCode + hostname).encode())
-
-        print('waiting for Syn/Ack')
-        data, address = sock.recvfrom(4096)
-        print('received {!r}'.format(data))
-
-        if data == (connectionCode + 'accept ' + hostname).encode():
-            print('Sending accept to server')
-            sock.send((connectionCode + 'accept').encode())
-            data, address = sock.recvfrom(4096)
-            print(data)
-            if data == 'Connection Established'.encode():
-                connected = True
-                threading.Thread(target=threadx).start()
-                threading.Thread(target=threadBeat).start()
-
-        message = ''
-        while connected and message != 'disconnect':
-            message = input()
-            sock.send(message.encode())
-            heartBeatIntervalCheck = 0
-
-    finally:
-        print('closing socket')
-        connected = False
-        sock.close()
-
 
 def threadBeat():
     global heartBeatIntervalCheck
 
     while heartBeat == "True" and connected:
         if heartBeatIntervalCheck == 3:
-            sock.send('con-h 0x00'.encode())
+            sock.sendto('con-h 0x00'.encode(), address)
             heartBeatIntervalCheck = 0
         else:
             time.sleep(1)
@@ -70,17 +33,44 @@ def threadBeat():
 def threadx():
     global connected
 
-    try:
-        while connected:
-            data, address = sock.recvfrom(4096)
-            if(data == "con-res 0xFE".encode()):
-                sock.send('con-res 0xFF'.encode())
-                print("YOU GOT FUCKED. timed out")
-                connected = False
-            else:
-                print(data)
-    except:
-        print("Listening thread closed")
+    while(True):
+        data, address = sock.recvfrom(4096)
+        if(data == "con-res 0xFE".encode()):
+            sock.sendto('con-res 0xFF'.encode(), server_address)
+            print("YOU GOT FUCKED. timed out")
+            connected = False
+            break
+        else:
+            print(data)
 
-if __name__ == "__main__":
-   main()
+
+try:
+    # Send data
+    print('Sending IP for Syn')
+    sock.sendto((connectionCode + IPAddr).encode(), server_address)
+
+    # Receive response
+    print('waiting for Syn/Ack')
+    data, address = sock.recvfrom(4096)
+    print('received {!r}'.format(data))
+
+    if data == (connectionCode + 'accept ' + IPAddr).encode():
+        print('Sending accept to server')
+        sock.sendto((connectionCode + 'accept').encode(), server_address)
+        data, address = sock.recvfrom(4096)
+        print(data)
+        if data == 'Connection Established'.encode():
+            connected = True
+            threading.Thread(target=threadx).start()
+            threading.Thread(target=threadBeat).start()
+
+    message = ''
+    while connected and message != 'disconnect':
+        message = input()
+        sock.sendto(message.encode(), address)
+        heartBeatIntervalCheck = 0
+
+
+finally:
+    print('closing socket')
+    sock.close()
--- Server.py
+++ Server.py
@@ -2,104 +2,80 @@ import socket
 from configparser import ConfigParser
 import time
 import threading
-import sys
 
 configur = ConfigParser()
 configur.read('opt.conf')
 packets = configur.get('ServerSettings', 'PackageSize')
 
-packetsSecond = 0
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+hostname = socket.gethostname()
+IPAddr = socket.gethostbyname(hostname)
+
+server_address = (IPAddr, 10000)
+print('starting up on {} port {}'.format(*server_address))
+sock.bind(server_address)
+
+
 connectionCode = 'com-0: '
 timeoutMsg = 'con-res 0xFE'
+SynAck = False
+res = -1
 timeout = 4
-
-
-def start_server():
-    hostname = socket.gethostbyname(socket.gethostname())
-    port = 10000
-    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-    try:
-        sock.bind((hostname, port))
-    except:
-        print("Bind failed. Error : " + str(sys.exc_info()))
-        sys.exit()
-
-    sock.listen(2)
-
-    while True:
-        print('\nWaiting for a client')
-        newConnection, address = sock.accept()
-        try:
-            threading.Thread(target=threadClient, args=(newConnection, hostname)).start()
-        except:
-            print("Thread did not start.")
-            sock.close()
-
-
-def threadClient(newConnection, hostname):
-    data, address = newConnection.recvfrom(4096)
-    print('received {} bytes from {}'.format(len(data), address))
-    print(data)
-
-    if data == (connectionCode + hostname).encode():
-        sent = newConnection.send((connectionCode + 'accept ' + hostname).encode())
-        print('sent {} bytes back to {}'.format(sent, address))
-        data, address = newConnection.recvfrom(4096)
-        if data == (connectionCode + 'accept').encode():
-            print(data)
-            print('Established connection to Client')
-            newConnection.send('Connection Established'.encode())
-            messageHandling(newConnection)
-    else:
-        sent = newConnection.send('Server connection denied'.encode())
-        print('sent {} bytes back to {}'.format(sent, address))
-        print('Denied Client connection access')
-        newConnection.close()
-
-
-def messageHandling(newConnection):
-    res = -1
-    global packetsSecond
-    threading.Thread(target=threadPacket).start()
-    while True:
-        newConnection.settimeout(timeout)
-        try:
-            if packetsSecond < int(packets):
-                print('\nWaiting for input...')
-                data, address = newConnection.recvfrom(4096)
-                if data == 'disconnect'.encode():
-                    newConnection.close()
-                    print("This client closed the connection")
-                    break
-                else:
-                    packetsSecond += 1
-                    print('Packets from client this second:')
-                    print(packetsSecond)
-                    res += 1
-                    message = 'msg-%s %s' % (res, data)
-                    res += 1
-                    response = 'res-%s - I am server' % res
-                    print(message)
-                    newConnection.send(response.encode())
-            else:
-                newConnection.send('Too many packages, please wait'.encode())
-                time.sleep(1)
-        except socket.timeout:
-            print('The Client timed out')
-            newConnection.send(timeoutMsg.encode())
-            data, address = newConnection.recvfrom(4096)
-            print(data)
-            newConnection.close()
-            break
+packetsSecond = 0
 
 
 def threadPacket():
     global packetsSecond
-
     while True:
         packetsSecond = 0
         time.sleep(1)
 
-if __name__ == "__main__":
-   start_server()
+while True:
+    SynAck = False
+    sock.settimeout(None)
+    print('\nWaiting for a client')
+    data, address = sock.recvfrom(4096)
+    print('received {} bytes from {}'.format(len(data), address))
+    print(data)
+
+    if data == (connectionCode + IPAddr).encode():
+        sent = sock.sendto((connectionCode + 'accept ' + IPAddr).encode(), address)
+        print('sent {} bytes back to {}'.format(sent, address))
+        data, address = sock.recvfrom(4096)
+        if data == (connectionCode + 'accept').encode():
+            print(data)
+            print('Established connection to Client')
+            sock.sendto('Connection Established'.encode(), address)
+            SynAck = True
+            threading.Thread(target=threadPacket).start()
+    else:
+        sent = sock.sendto('Server connection denied'.encode(), address)
+        print('sent {} bytes back to {}'.format(sent, address))
+        print('Denied Client connection access')
+        SynAck = False
+
+    while SynAck:
+        sock.settimeout(timeout)
+        try:
+            if packetsSecond < int(packets):
+                print('\nWaiting for input...')
+                data, address = sock.recvfrom(4096)
+                packetsSecond += 1
+                print('Packets this second:')
+                print(packetsSecond)
+                res += 1
+                message = 'msg-%s %s' % (res, data)
+                res += 1
+                response = 'res-%s - I am server' % res
+                print(message)
+                sock.sendto(response.encode(), address)
+            else:
+                sock.sendto('Too many packages, please wait'.encode(), address)
+                time.sleep(1)
+        except socket.timeout:
+            print('The Client timed out')
+            sock.sendto(timeoutMsg.encode(), address)
+            data, address = sock.recvfrom(4096)
+            print(data)
+            break
